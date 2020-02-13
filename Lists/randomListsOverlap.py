17738d9adf4b4a92ba6214b6f4379e76025ed035
viktor <veterpavlovich@gmail.com>
2020-02-14 00:37:20 +0100

    remove unnecessary variable

--- Lists/randomListsOverlap.py
+++ Lists/randomListsOverlap.py
@@ -1,32 +1,25 @@
 import random
 
 numberOfLists = int(input("Enter number of lists : "))
+listLength = 10  # arbitrary choosen
 
 lists = []
 while numberOfLists > 0:
     numberOfLists -= 1
     l = []
-    for y in range(10):
+    for y in range(listLength):
         l.append(random.choice(range(7)))
 
     lists.append(l)
-
     print(l)
 
 resultList = []
 
 for el in lists[0]:
-    isInAllLists = True
-
-    for l_ in lists:
-        if el not in l_:
-            isInAllLists = False
-
+    for subList in lists:
+        if el not in subList:
             break
-
-    if isInAllLists and (el not in resultList):
-
+    if el in subList and (el not in resultList):
         resultList.append(el)
-
 
 print("The result list: ", resultList)
